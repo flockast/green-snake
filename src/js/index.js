a6172708d164d5a2821f3775c538d932983591de
Константин Власов <kvlasov@stdev.ru>
2023-08-10 12:24:08 +0300

    init

---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+import { initSnakeGame } from './snake/index.js'
+
+initSnakeGame(document.querySelector('#canvas-snake'))
---
+++ src/js/snake/index.js
@@ -0,0 +1,3 @@
+export const initSnakeGame = (element) => {
+  element.style.background = 'green'
+}
